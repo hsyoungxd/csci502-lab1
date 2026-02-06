be0976505f22540b4f7324088b639256824f1a1c
hsy <hsyoungxd@gmail.com>
2026-02-06 11:41:54 +0500

    lab1

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Raspberry Pi IMU project!" << endl;
+    cout << "C++ Git example is working correctly." << endl;
+    return 0;
+}
